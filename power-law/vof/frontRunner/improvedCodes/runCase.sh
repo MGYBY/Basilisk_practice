3bcb681d35553273fe9b86fbcbf0901c220a184f
BYY <53412286+MGYBY@users.noreply.github.com>
2023-11-10 11:43:19 -0500

    Update runCase.sh

--- power-law/vof/frontRunner/improvedCodes/runCase.sh
+++ power-law/vof/frontRunner/improvedCodes/runCase.sh
@@ -1,4 +1,9 @@
 CC99='mpicc -std=c99' qcc -Wall -O2 -Wdimensions -D_MPI=1 rw.c -o rw -lm -L$BASILISK/gl -lglutils -lfb_osmesa -lOSMesa -lGLU -lm
 mpirun --oversubscribe -np 14 ./rw -parallel > log
 
+######### using OpenMP #########
+# export OMP_NUM_THREADS=13
+# qcc  -std=c99 -fopenmp -Wall -O2 roll-wave.c -o rollwave -lm -grid=octree -L$BASILISK/gl -lglutils -lfb_tiny
+# ./rollwave
+
 # make rw.tst
