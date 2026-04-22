ec0f4df7f6f5d7bc8b68f49b9f67732c81cc4be6
BYY <53412286+MGYBY@users.noreply.github.com>
2026-04-21 23:54:53 -0400

    Add files via upload

---
+++ rw_thixo/periodic/VOF/runCaseSimple.sh
@@ -0,0 +1,5 @@
+python3 thixo_base_state_explicitFrV_fixed.py --kappa 1e-4 --a 0.2 --Gamma 8 --FrV 4.00 --n-rows 128 --output profile.txt
+
+export OMP_NUM_THREADS=5
+qcc  -std=c99 -fopenmp -Wall -O2 -Wdimensions thixo_periodic_rollwave.c -o rollwave -lm -grid=quadtree -L$BASILISK/gl -lglutils -lfb_tiny
+./rollwave
